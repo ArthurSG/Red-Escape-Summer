294c83d57732f8122d7667782812b7f18f7c37da
skitak <bas2205@live.fr>
2019-07-18 17:52:58 +0200

    collider adder

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public int minFov = 60;
-    public int maxFov = 90;
+    public float minFov = 60;
+    public float maxFov = 90;
+    Camera cam;
+    void Start() {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update() {
+        float interpolation = (GameManager.instance.GetSpeed() - GameManager.instance.minSpeed) / GameManager.instance.maxSpeed;
+        cam.fieldOfView = Mathf.Lerp(minFov, maxFov, interpolation);
+        print(interpolation);
+    }
 }
